1b0fa4d8b578241d103c8b9e69cd3b4f2108f22c
Bibhuti <bibhuti103singh@gmail.com>
2023-06-25 19:45:34 +0530

    feat: add info modal description

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -31,13 +31,32 @@
             <h1 class="text-2xl">About</h1>
           </template>
           <template #info>
-            <p class="text-black mb-4">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id
-              inventore iure et aspernatur neque itaque vitae repellat ea
-              similique ad excepturi odit asperiores quo facilis placeat
-              voluptatibus, sunt cupiditate recusandae ratione, sit laborum iste
-              labore! Porro ipsa facilis quod modi!
+            <p class="text-black mb-8 font-bold text-lg">
+              Welcome to the Weather Tracking App! This app allows you to stay
+              updated on the weather conditions for cities across the globe.
+              Here's how it works:
             </p>
+            <div>
+              <ul class="text-black list-disc ml-4 mb-5 italic">
+                <li>
+                  Search for a city: Use the search feature to find the city
+                  you're interested in.
+                </li>
+                <li>
+                  Select city from search results: Once you find the desired
+                  city in the search results, click on it to select it.
+                </li>
+                <li>
+                  Add city to tracking list: By clicking on the "+" sign, you
+                  can add the selected city to your tracking list.
+                </li>
+                <li>
+                  Delete tracking city: If you want to remove a city from your
+                  tracking list, simply open the city's details and select the
+                  delete button.
+                </li>
+              </ul>
+            </div>
           </template>
         </AlertModal>
       </div>
@@ -60,3 +79,9 @@ defineEmits(["on-open-modal", "on-click-add"]);
 
 const route = useRoute();
 </script>
+
+<style scoped>
+li {
+  margin-bottom: 5px;
+}
+</style>
